cfaa6910ecbc1970a7cccd502d3e82f87a724e97
Rahul kumar maury <rahu7827759@gmail.com>
2024-01-30 11:11:35 +0530

    Retrofit api (complete get request)

--- app/src/main/java/com/programmingz/newsapp/util/Constants.kt
+++ app/src/main/java/com/programmingz/newsapp/util/Constants.kt
@@ -2,6 +2,9 @@ package com.programmingz.newsapp.util
 
 class Constants {
     companion object{
-        const val API_KEY = "com.programmingz.newsapp.models.Article"
+        const val API_KEY = "03cff01bfcf4418d9011a8a2d6b61bad"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEW_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
     }
 }
