6ceee53596575e9587b468a1fa7731799167100a
Rahul kumar maury <rahu7827759@gmail.com>
2024-02-01 14:55:34 +0530

    Retrofit api (complete get request) set Room Database add repository

---
+++ app/src/main/java/com/programmingz/newsapp/repository/NewsRepository.kt
@@ -0,0 +1,20 @@
+package com.programmingz.newsapp.repository
+
+import com.programmingz.newsapp.api.RetrofitInstance
+import com.programmingz.newsapp.db.ArticleDatabase
+import com.programmingz.newsapp.models.Article
+
+class NewsRepository(val db: ArticleDatabase) {
+
+    suspend fun getHeadlines(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getHeadlines(countryCode, pageNumber)
+
+    suspend fun getSearchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getFavouriteNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+}
